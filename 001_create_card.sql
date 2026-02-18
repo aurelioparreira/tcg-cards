0ccf16309902fdc94f57a022b84b5342580fa484
aurelioparreira <profaurelioparreira@gmail.com>
2026-02-18 20:48:57 -0300

    Add files via upload

---
+++ 001_create_card.sql
@@ -0,0 +1,38 @@
+CREATE TABLE tbl_collections (
+    id SERIAL PRIMARY KEY,
+    collectionSetName VARCHAR(100) NOT NULL,
+    releaseDate DATE NOT NULL,
+    totalCardsInCollection SMALLINT NOT NULL
+);
+
+CREATE TABLE tbl_types (
+    id SERIAL PRIMARY KEY,
+    typeName VARCHAR(30) NOT NULL UNIQUE
+);
+
+CREATE TABLE tbl_stages (
+    id SERIAL PRIMARY KEY,
+    stageName VARCHAR(20) NOT NULL UNIQUE
+);
+
+CREATE TABLE tbl_cards (
+    id SERIAL PRIMARY KEY,
+    hp SMALLINT,
+    name VARCHAR(80) NOT NULL,
+    info TEXT,
+    weak VARCHAR(30),
+    resis VARCHAR(30),
+    retreat VARCHAR(15),
+    cardNumberInCollection SMALLINT NOT NULL,
+    collection_id INT NOT NULL,
+    type_id INT NOT NULL,
+    stage_id INT NOT NULL,
+    CONSTRAINT fk_collection FOREIGN KEY (collection_id)
+        REFERENCES tbl_collections (id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_type FOREIGN KEY (type_id)
+        REFERENCES tbl_types (id),
+    CONSTRAINT fk_stage FOREIGN KEY (stage_id)
+        REFERENCES tbl_stages (id),
+    CONSTRAINT uq_card_number UNIQUE (collection_id, cardNumberInCollection)
+);
